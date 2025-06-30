6c159cd7b15e41568c98202427d014661b5644a9
Rakesh Choudhary <seervir987@gmail.com>
2025-06-30 11:51:50 +0530

    Create background AI jobs.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,26 @@
 "use client";
 
 import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
 import { useTRPC } from "@/trpc/client";
 import { useMutation } from "@tanstack/react-query";
+import { useState } from "react";
 
 const Page = () => {
+  const [value, setValue] = useState("");
+
   const trpc = useTRPC();
   const invoke = useMutation(trpc.invoke.mutationOptions({}));
 
   return (
     <div className="flex flex-col items-center justify-center h-screen">
-      <Button onClick={() => invoke.mutate({ text: "world" })}>
+      <Button
+        disabled={invoke.isPending}
+        onClick={() => invoke.mutate({ value: value })}
+      >
         Start the Ingest
       </Button>
+      <Input value={value} onChange={(e) => setValue(e.target.value)} />
     </div>
   );
 };
--- src/inngest/functions.ts
+++ src/inngest/functions.ts
@@ -1,10 +1,20 @@
 import { inngest } from "./client";
+import { createAgent, gemini } from "@inngest/agent-kit";
 
 export const helloWorld = inngest.createFunction(
   { id: "hello-world" },
   { event: "test/hello.world" },
-  async ({ event, step }) => {
-    await step.sleep("wait-a-moment", "10s");
-    return { message: `Hello ${event.data.email}!` };
-  },
+  async ({ event }) => {
+    const codeWriterAgent = createAgent({
+      name: "Code writer",
+      system:
+        "you are a code writer.you are expert in nextjs and react.js.You have to write the code snippets based on the user input. For styling use tailwind css.",
+      model: gemini({ model: "gemini-2.0-flash" }),
+    });
+    const { output } = await codeWriterAgent.run(
+      `write a simple nextjs snippet based on the input provided : ${event.data.value}`
+    );
+    console.log("Output from agent:", output);
+    return { output };
+  }
 );
