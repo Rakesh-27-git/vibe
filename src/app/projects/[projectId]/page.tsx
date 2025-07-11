359c917df2ea0488a1aa488f3c175430f719ea2b
Rakesh Choudhary <seervir987@gmail.com>
2025-07-11 20:19:06 +0530

    create message ui

--- src/app/projects/[projectId]/page.tsx
+++ src/app/projects/[projectId]/page.tsx
@@ -1,17 +1,35 @@
-import React from "react";
+import React, { Suspense } from "react";
+
+import { getQueryClient, trpc } from "@/trpc/server";
+import { dehydrate, HydrationBoundary } from "@tanstack/react-query";
+
+import ProjectView from "@/modules/projects/ui/views/project-view";
 
 interface props {
-  params: {
-    projectId: string;
-  };
+  params: Promise<{ projectId: string }>;
 }
 
-const Page = ({ params }: props) => {
-  const { projectId } = params;
+const Page = async ({ params }: props) => {
+  const { projectId } = await params;
+
+  const queryClient = getQueryClient();
+
+  await queryClient.prefetchQuery(
+    trpc.projects.getOne.queryOptions({
+      id: projectId,
+    })
+  );
+  void queryClient.prefetchQuery(
+    trpc.messages.getMany.queryOptions({
+      projectId,
+    })
+  );
   return (
-    <div>
-      <h1>Project Id: {projectId}</h1>
-    </div>
+    <HydrationBoundary state={dehydrate(queryClient)}>
+      <Suspense fallback={<div>Loading...</div>}>
+        <ProjectView projectId={projectId} />
+      </Suspense>
+    </HydrationBoundary>
   );
 };
 
--- src/modules/projects/server/procedures.ts
+++ src/modules/projects/server/procedures.ts
@@ -4,16 +4,35 @@ import { generateSlug } from "random-word-slugs";
 import { inngest } from "@/inngest/client";
 import { prisma } from "@/lib/prisma";
 import { baseProcedure, createTRPCRouter } from "@/trpc/init";
+import { TRPCError } from "@trpc/server";
 
 export const projectsRouter = createTRPCRouter({
+  getOne: baseProcedure
+    .input(
+      z.object({
+        id: z.string().min(1, { message: "Project ID is required" }),
+      })
+    )
+    .query(async ({ input }) => {
+      const project = await prisma.project.findUnique({
+        where: {
+          id: input.id,
+        },
+      });
+      if (!project) {
+        throw new TRPCError({
+          code: "NOT_FOUND",
+          message: "Project not found",
+        });
+      }
+      return project;
+    }),
+
   getMany: baseProcedure.query(async () => {
     const projects = await prisma.project.findMany({
       orderBy: {
         createdAt: "desc",
       },
-      //   include: {
-      //     fragment: true
-      //   }
     });
     return projects;
   }),
